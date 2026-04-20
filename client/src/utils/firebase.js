98c721c6e000d594800d408f7e7edd1dfa08ab28
ruchitarkg05@gmail.com <ruchitarkg05@gmail.com>
2026-04-21 04:25:59 +0530

    fix: update Firebase config

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,15 +1,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+//   authDomain: "prepbot-64a8d.firebaseapp.com",
+//   projectId: "prepbot-64a8d",
+//   storageBucket: "prepbot-64a8d.firebasestorage.app",
+//   messagingSenderId: "963144046648",
+//   appId: "1:963144046648:web:2d6ce86b6767a55d7faecd"
+// };
+
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "prepbot-64a8d.firebaseapp.com",
-  projectId: "prepbot-64a8d",
-  storageBucket: "prepbot-64a8d.firebasestorage.app",
-  messagingSenderId: "963144046648",
-  appId: "1:963144046648:web:2d6ce86b6767a55d7faecd"
+  authDomain: "prepbot-4e9ad.firebaseapp.com",
+  projectId: "prepbot-4e9ad",
+  storageBucket: "prepbot-4e9ad.firebasestorage.app",
+  messagingSenderId: "321776809940",
+  appId: "1:321776809940:web:52d1286298766be5974841"
 };
-
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
